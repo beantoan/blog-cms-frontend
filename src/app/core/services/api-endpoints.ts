2a374db9fdff85069d32d2abb62540fa51ac4722
Đậu Quốc Toản <beantoan@gmail.com>
2019-02-07 01:40:29 +0100

    Added posts API endpoint

--- src/app/core/services/api-endpoints.ts
+++ src/app/core/services/api-endpoints.ts
@@ -3,5 +3,5 @@ export class ApiEndpoints {
   static USER_LOGIN = '/api/auth/login';
   static USER_ME = '/api/auth/me';
   static USER_LOGOUT = '/api/auth/logout';
-  static POSTS = '/posts';
+  static POSTS = '/api/posts';
 }
